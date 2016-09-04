a73a95e10d34e22eb3d55641b27bc1e700818b8f
Jonny Burger <jonathanburger11@gmail.com>
2016-09-04 16:34:41 +0200

    Add linux support

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 'use strict';
 const execa = require('execa');
 
+const getBinaryName = () => {
+	if (process.platform === 'darwin') {
+		return 'pandoc-osx';
+	}
+	if (process.platform === 'linux') {
+		return 'pandoc-linux';
+	}
+	throw new Error('Module is only supported on macOS and Linux');
+}
+
 module.exports = (args, options) => {
 	return new Promise((resolve, reject) => {
-		execa('./bin/pandoc', args, options)
+		execa(`./bin/${getBinaryName()}`, args, options)
 		.then(result => {
 			if (result.stderr.length) {
 				return reject(result.stderr);
